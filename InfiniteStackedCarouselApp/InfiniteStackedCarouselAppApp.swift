4890c81a1c20b18d2ae34032231d6088dc4b25a4
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-02-24 13:51:47 +0900

    Initial Commit

---
+++ InfiniteStackedCarouselApp/InfiniteStackedCarouselAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteStackedCarouselAppApp.swift
+//  InfiniteStackedCarouselApp
+//
+//  Created by Seungchul Ha on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteStackedCarouselAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
